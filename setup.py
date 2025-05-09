9e17580312d755ed1950ea325d920d0b6d90fd48
Enrique Steck <150300119@ucaribe.edu.mx>
2025-05-09 13:47:10 -0500

    primer lanzamiento de paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repoMypip3",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Enrique Ochoa",                         # Tu nombre
+    author_email="eochoa@vivo.mx",                 # Tu correo electrónico
+    url="https://github.com/steck3/repoMypip3",     # URL del proyecto
+)
